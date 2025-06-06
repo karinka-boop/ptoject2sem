568a28c56ff3a0d2d043fb0b8e15aaa84c324f86
karinka-boop <kbaimukhamedova06@gmail.com>
2025-06-06 10:35:55 +0300

    Add files via upload

---
+++ shop/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Product, Category
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'price', 'category', 'image', 'is_active', 'discount']
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 3}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['category'].queryset = Category.objects.all()
